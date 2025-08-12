805ad41d225c1e7acc3e7f18bd00df005eb42984
dl <laughinglucky6@gmail.com>
2025-08-12 21:12:12 +0800

    nvim config update

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,11 +4,11 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.diagnostics.erb_lint,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
+				-- null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.diagnostics.clangd,
+				null_ls.builtins.formatting.clangd,
 			},
 		})
 
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,23 +4,27 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.g.background = "light"
-vim.keymap.set('n','<Leader>w', ':w<CR>')
-vim.keymap.set('n','<Leader>q', ':q<CR>')
+vim.keymap.set("n", "<Leader>w", ":w<CR>")
+vim.keymap.set("n", "<Leader>q", ":q<CR>")
 vim.cmd("set mouse=v")
 
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("set nu")
+vim.cmd("set relativenumber")
 vim.opt.swapfile = false
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
 
-vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
-
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({"FocusGained", "BufEnter", "CursorHold", "CursorHoldI"}, {
+  command = "if mode() != 'c' | checktime | endif"
+})
