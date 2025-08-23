e834a5d47d85ddb0e14c6ad4d7e84a3a612c77aa
dl <laughinglucky6@gmail.com>
2025-08-23 12:54:01 +0800

    fix none-ls bug, add none-ls-extra; add folder action

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,8 +12,14 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.runtimepath:prepend(lazypath)
+-- vim python
 vim.g.python3_host_prog = " /Users/dl/.pyenv/shims/python"
 
+-- vim folder
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldlevel = 99        -- Open folds by default
+vim.opt.foldenable = false    -- Don't fold automatically on open
 
 require("vim-options")
 
